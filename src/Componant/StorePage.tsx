bd037dd499169d45198945939e9ad80a46a2a966
HossamUK <hossam.m.pro@gmail.com>
2021-02-02 22:49:02 -0800

    update

--- src/Componant/StorePage.tsx
+++ src/Componant/StorePage.tsx
@@ -5,6 +5,7 @@ import { AiOutlineInstagram } from "react-icons/ai";
 import { AiFillFacebook } from "react-icons/ai";
 import { AiOutlineTwitter } from "react-icons/ai";
 import { FaVimeoV } from "react-icons/fa";
+import { url } from "inspector";
 
 export default function StorePage() {
   const [rotate, setRotate] = useState(false);
@@ -15,6 +16,7 @@ export default function StorePage() {
     { name: "f" },
 
     { name: "f" },
+    { name: "f", cal: true },
     { name: "f" },
     { name: "f" },
     { name: "f" },
@@ -75,10 +77,48 @@ export default function StorePage() {
         <div className="store_grid">
           {piece.map((it: any) => {
             return (
-              <div className="grid_card">
-                <h3>Manz Sport</h3>
-                <p>$ 39.99</p>
-                <img src="/assats/manBoots/MnzWhite.jpg" alt="White Boot"></img>
+              <div
+                style={it.cal && { backgroundImage: "url(/assats/cal3.jpg)" }}
+                className={`grid_card ${it.cal && "grid_cardCal"}`}
+              >
+                {it.cal ? (
+                  <div className="card_cal">
+                    <div>
+                      <span>C</span>
+                      <span>ollections</span>
+                    </div>
+                    <button>
+                      <div className="grid_discover">
+                        <div>
+                          <span>D</span>
+                          <span>iscover</span>
+                        </div>
+
+                        <svg
+                          width="19"
+                          height="9"
+                          viewBox="0 0 26 16"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M25.7071 8.70711C26.0976 8.31658 26.0976 7.68342 25.7071 7.29289L19.3431 0.928932C18.9526 0.538408 18.3195 0.538408 17.9289 0.928932C17.5384 1.31946 17.5384 1.95262 17.9289 2.34315L23.5858 8L17.9289 13.6569C17.5384 14.0474 17.5384 14.6805 17.9289 15.0711C18.3195 15.4616 18.9526 15.4616 19.3431 15.0711L25.7071 8.70711ZM0 9H25V7H0V9Z"
+                            fill="#232323"
+                          />
+                        </svg>
+                      </div>
+                    </button>
+                  </div>
+                ) : (
+                  <div className="card_pro">
+                    <h3>Manz Sport</h3>
+                    <p>$ 39.99</p>
+                    <img
+                      src="/assats/manBoots/MnzWhite.jpg"
+                      alt="White Boot"
+                    ></img>
+                  </div>
+                )}
               </div>
             );
           })}
